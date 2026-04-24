10ef7e433bbd88fbf38ae2b00efea674738c8a3e
Ismail Sheikh <129026460+Ismail26477@users.noreply.github.com>
2026-04-25 02:30:33 +0530

    Update MedicalCondition.ts

--- server/models/MedicalCondition.ts
+++ server/models/MedicalCondition.ts
@@ -1,7 +1,7 @@
-import mongoose, { Schema, Document } from 'mongoose';
+import mongoose, { Schema, Document, ObjectId } from 'mongoose';
 
 export interface IMedicalCondition extends Document {
-  _id: string;
+  _id?: ObjectId;
   userId: string;
   name: string;
   status: 'active' | 'resolved';
